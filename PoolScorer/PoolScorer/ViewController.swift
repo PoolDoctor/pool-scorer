b5d655f3b1ba3f24257cecb4b6e716ec899b84fa
Abhinav Rai <abhinav_rai@riverbed.com>
2016-11-07 21:23:25 -0800

    Adding empty xcode project

---
+++ PoolScorer/PoolScorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PoolScorer
+//
+//  Created by Abhinav Rai on 11/7/16.
+//  Copyright © 2016 PoolDoctor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
