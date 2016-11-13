e63a3ece42c17a05abdf4af74f4f92d21f150e13
Abhinav Rai <abhinav_rai@riverbed.com>
2016-11-12 18:28:22 -0800

    Added skeleton view controllers and associated classes

---
+++ PoolScorer/PoolScorer/LeagueMatchInfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LeagueMatchInfoViewController.swift
+//  PoolScorer
+//
+//  Created by Abhinav Rai on 11/12/16.
+//  Copyright © 2016 PoolDoctor. All rights reserved.
+//
+
+import UIKit
+
+class LeagueMatchInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
