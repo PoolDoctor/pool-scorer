7ac6df8dccd5254ca2d88e4b9c023eeaf4fb78dd
limenuke <consid3r@gmail.com>
2016-12-05 20:04:14 -0800

    Adding deadball and innings

--- PoolScorer/PoolScorer/SingleMatchScoringViewController.swift
+++ PoolScorer/PoolScorer/SingleMatchScoringViewController.swift
@@ -35,6 +35,85 @@ class SingleMatchScoringViewController: UIViewController, ScoringViewDelegate {
         }
     }
     
+ 
+    @IBOutlet weak var currInning: UILabel!
+    @IBOutlet weak var deadBallCt: UILabel!
+    @IBOutlet weak var endInningButton: UIButton!
+    @IBOutlet weak var deadBallButton: UIButton!
+    @IBOutlet weak var prevFrameButton: UIButton!
+    @IBOutlet weak var nextFrameButton: UIButton!
+    @IBOutlet weak var p1scoreView: ScoringView!
+    @IBOutlet weak var p2scoreView: ScoringView!
+    var match : SingleMatch?
+    var currFrameNo : Int!
+    var currentFrame : Frame?
+    
+    @IBOutlet weak var frameNoLabel: UILabel!
+    @IBOutlet weak var frameStatusLabel: UILabel!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        if (match!.status == .Unstarted) {
+            match!.status = .Ongoing
+        }
+        if ((match?.frames.count)! == 0) {
+            currentFrame = Frame(p1Needs: pointsNeeded(skill: (match?.hostPlayer.skillLevel)!), p2Needs: pointsNeeded(skill: (match?.visitingPlayer.skillLevel)!), p1TimeOutsAllowed: 2, p2TimeOutsAllowed: 2)
+            match?.frames.append(currentFrame!)
+            currFrameNo = 1
+        } else {
+            currFrameNo = match?.frames.count
+            currentFrame = match?.currentFrame
+        }
+        prevFrameButton.layer.cornerRadius = 2.0
+        prevFrameButton.layer.masksToBounds = true
+        nextFrameButton.layer.cornerRadius = 2.0
+        nextFrameButton.layer.masksToBounds = true
+        endInningButton.layer.cornerRadius = 2.0
+        endInningButton.layer.masksToBounds = true
+        deadBallButton.layer.cornerRadius = 2.0
+        deadBallButton.layer.masksToBounds = true
+        
+        reloadViews()
+    }
+    
+    func reloadViews () {
+        p1scoreView.playerName.text = match?.player1.firstName
+        p2scoreView.playerName.text = match?.player2.firstName
+        p1scoreView.playerSkill.text = String(describing: match!.player1.skillLevel)
+        p2scoreView.playerSkill.text = String(describing: match!.player2.skillLevel)
+        
+        p1scoreView.scoreLabel.text = String(describing: currentFrame!.p1Score)
+        p2scoreView.scoreLabel.text = String(describing: currentFrame!.p2Score)
+        
+        p1scoreView.defenseLabel.text = String(describing: match!.p1Defenses)
+        p2scoreView.defenseLabel.text = String(describing: match!.p2Defenses)
+        
+        p1scoreView.scoreIncButton.addTarget(self, action: #selector(p1changeScore), for: UIControlEvents.touchUpInside)
+        p1scoreView.scoreDecButton.addTarget(self, action: #selector(p1changeScore), for: UIControlEvents.touchUpInside)
+        p1scoreView.defenseIncButton.addTarget(self, action: #selector(p1changeDef), for: UIControlEvents.touchUpInside)
+        p1scoreView.defenseDecButton.addTarget(self, action: #selector(p1changeDef), for: UIControlEvents.touchUpInside)
+        
+        p2scoreView.scoreIncButton.addTarget(self, action: #selector(p2changeScore), for: UIControlEvents.touchUpInside)
+        p2scoreView.scoreDecButton.addTarget(self, action: #selector(p2changeScore), for: UIControlEvents.touchUpInside)
+        p2scoreView.defenseIncButton.addTarget(self, action: #selector(p2changeDef), for: UIControlEvents.touchUpInside)
+        p2scoreView.defenseDecButton.addTarget(self, action: #selector(p2changeDef), for: UIControlEvents.touchUpInside)
+        
+        frameNoLabel.text = String(describing: currFrameNo!)
+        if (currentFrame?.frameEnded == true) {
+            frameStatusLabel.text = "Ended"
+        } else {
+            frameStatusLabel.text = "In Progress"
+        }
+        currInning.text = String(describing:currentFrame!.innings)
+        deadBallCt.text = String(describing:currentFrame!.deadBallCount)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     @IBAction func onPrev(_ sender: Any) {
         print ("Prev button!")
         if (currFrameNo > 1) {
@@ -45,6 +124,18 @@ class SingleMatchScoringViewController: UIViewController, ScoringViewDelegate {
             reloadViews()
         }
     }
+    
+    
+    @IBAction func onDeadBall(_ sender: Any) {
+        match?.currentFrame.incDeadBallCount()
+        reloadViews()
+    }
+    
+    @IBAction func onEndInning(_ sender: Any) {
+        match?.currentFrame.incInnings()
+        reloadViews()
+    }
+    
     @IBAction func onNext(_ sender: Any) {
         print ("Next button!")
         if (currentFrame?.frameEnded == true) {
@@ -86,76 +177,14 @@ class SingleMatchScoringViewController: UIViewController, ScoringViewDelegate {
                 self.currentFrame = Frame(p1Needs: self.pointsNeeded(skill: (self.match?.hostPlayer.skillLevel)!), p2Needs: self.pointsNeeded(skill: (self.match?.visitingPlayer.skillLevel)!), p1TimeOutsAllowed: 2, p2TimeOutsAllowed: 2)
                 self.match?.frames.append(self.currentFrame!)
                 self.currFrameNo = self.match?.frames.count
-
+                
             }
             alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default,handler: yesHandler))
             alert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.default,handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
     }
-    @IBOutlet weak var prevFrameButton: UIButton!
-    @IBOutlet weak var nextFrameButton: UIButton!
-    @IBOutlet weak var p1scoreView: ScoringView!
-    @IBOutlet weak var p2scoreView: ScoringView!
-    var match : SingleMatch?
-    var currFrameNo : Int!
-    var currentFrame : Frame?
     
-    @IBOutlet weak var frameNoLabel: UILabel!
-    @IBOutlet weak var frameStatusLabel: UILabel!
-    override func viewDidLoad() {
-        
-        super.viewDidLoad()
-        if (match!.status == .Unstarted) {
-            match!.status = .Ongoing
-        }
-        if ((match?.frames.count)! == 0) {
-            currentFrame = Frame(p1Needs: pointsNeeded(skill: (match?.hostPlayer.skillLevel)!), p2Needs: pointsNeeded(skill: (match?.visitingPlayer.skillLevel)!), p1TimeOutsAllowed: 2, p2TimeOutsAllowed: 2)
-            match?.frames.append(currentFrame!)
-            currFrameNo = 1
-        } else {
-            currFrameNo = match?.frames.count
-            currentFrame = match?.currentFrame
-        }
-        reloadViews()
-    }
-    
-    func reloadViews () {
-        p1scoreView.playerName.text = match?.player1.firstName
-        p2scoreView.playerName.text = match?.player2.firstName
-        p1scoreView.playerSkill.text = String(describing: match!.player1.skillLevel)
-        p2scoreView.playerSkill.text = String(describing: match!.player2.skillLevel)
-        
-        p1scoreView.scoreLabel.text = String(describing: currentFrame!.p1Score)
-        p2scoreView.scoreLabel.text = String(describing: currentFrame!.p2Score)
-        
-        p1scoreView.defenseLabel.text = String(describing: match!.p1Defenses)
-        p2scoreView.defenseLabel.text = String(describing: match!.p2Defenses)
-        
-        p1scoreView.scoreIncButton.addTarget(self, action: #selector(p1changeScore), for: UIControlEvents.touchUpInside)
-        p1scoreView.scoreDecButton.addTarget(self, action: #selector(p1changeScore), for: UIControlEvents.touchUpInside)
-        p1scoreView.defenseIncButton.addTarget(self, action: #selector(p1changeDef), for: UIControlEvents.touchUpInside)
-        p1scoreView.defenseDecButton.addTarget(self, action: #selector(p1changeDef), for: UIControlEvents.touchUpInside)
-        
-        p2scoreView.scoreIncButton.addTarget(self, action: #selector(p2changeScore), for: UIControlEvents.touchUpInside)
-        p2scoreView.scoreDecButton.addTarget(self, action: #selector(p2changeScore), for: UIControlEvents.touchUpInside)
-        p2scoreView.defenseIncButton.addTarget(self, action: #selector(p2changeDef), for: UIControlEvents.touchUpInside)
-        p2scoreView.defenseDecButton.addTarget(self, action: #selector(p2changeDef), for: UIControlEvents.touchUpInside)
-        
-        frameNoLabel.text = String(describing: currFrameNo!)
-        if (currentFrame?.frameEnded == true) {
-            frameStatusLabel.text = "Ended"
-        } else {
-            frameStatusLabel.text = "In Progress"
-        }
-            
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     func p1changeScore(sender: UIButton) {
         if (sender.titleLabel?.text == "+") {
